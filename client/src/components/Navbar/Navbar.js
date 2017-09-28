0182c6f0be28e1c6caf07223889196731c9d68c5
Andrea Pinillos <andreapinillos1@gmail.com>
2017-09-27 22:11:47 -0400

    too many files

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+const Navbar = props =>
+  <nav className="navbar navbar-default">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <Link className="navbar-brand" to="/">
+          Sidekick
+        </Link>
+      </div>
+      <ul className="nav navbar-nav">
+        <li
+          className={
+            window.location.pathname === "/" ||
+            window.location.pathname === "/about"
+              ? "active"
+              : ""
+          }
+        >
+          <Link to="/">About</Link>
+        </li>
+        <li
+          className={window.location.pathname === "/discover" ? "active" : ""}
+        >
+          <Link to="/discover">Discover</Link>
+        </li>
+        <li className={window.location.pathname === "/search" ? "active" : ""}>
+          <Link to="/search">Search</Link>
+        </li>
+                <li
+          className={window.location.pathname === "/Login" ? "active" : ""}
+        >
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </div>
+  </nav>;
+
+export default Navbar;
