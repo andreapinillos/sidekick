4c4db034d05714248acd07857e0a16fee0c62827
Dain Lee <dlekdls1028@gmail.com>
2017-10-03 13:21:45 -0400

    footer changed

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 const Footer = () =>
   <footer className="footer">
-    <span>Sidekick &copy; 2017 Aimila M, Anstasia, Andrea P, Dain L, Steven R </span>
+    <span>Sidekick &copy; 2017 Aimila, Anastasia, Andrea, Dain, Steven</span>
   </footer>;
 
 export default Footer;
