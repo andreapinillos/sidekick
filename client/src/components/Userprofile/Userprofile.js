ab160aa56c40d370544ef27853d04ca37f0347fb
Steven Rojsuontikul <steven.rojsuontikul@gmail.com>
2017-10-03 10:17:28 -0400

    user page

---
+++ client/src/components/Userprofile/Userprofile.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Col from "../Col";
+import Container from "../Container";
+import "./Userprofile.css";
+
+const Userprofile = props =>
+ 
+  <Container>
+
+    <div className="row">
+
+        <div className="col-sm-4">
+          Profile Pic
+        </div>
+
+        <div className="col-sm-8">
+          <h4>Name</h4>
+          <p>Keep up the GREAT work! I am cheering for you!! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <br>
+        </div>
+
+    </div>
+   <Container />
+
+
+
+export default Userprofile;
