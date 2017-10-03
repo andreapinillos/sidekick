643149390b5e3819f24564699661f42681ad919b
Andrea Pinillos <andreapinillos1@gmail.com>
2017-10-03 13:55:04 -0400

    footer and fb details

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 const Footer = () =>
   <footer className="footer">
-    <span>Sidekick &copy; 2017 Aimila, Anastasia, Andrea, Dain, Steven</span>
+    <span>Sidekick &copy; 2017 Aimila Maimaitijiang, Anastasia Mark, Andrea Pinillos, Dain Lee, Steven Rojsuontikul</span>
   </footer>;
 
 export default Footer;
