a111e3c771522ec5da74499260efafd05d654263
sharklemon <anastasia.l.mark@gmail.com>
2017-10-03 15:07:19 -0400

    added card href

--- client/src/components/People/People.js
+++ client/src/components/People/People.js
@@ -6,24 +6,26 @@ const People = props =>
 
 <div>
     <div className="col-sm-4">
-      <div className="cardbox">
-        <div className="card">
-          <img className="profimg" alt={props.name} src={props.image} />
-        </div>
-          <div className="card-block">
-            <ul>
-              <li>
-                <strong>Name:</strong> {props.name}
-              </li>
-              <li>
-                <strong>Activity:</strong> {props.activity}
-              </li>
-              <li>
-                <strong>Bio:</strong> {props.bio}
-              </li>
-            </ul>
+      <a href={props.href}>
+        <div className="cardbox">
+          <div className="card">
+            <img className="profimg" alt={props.name} src={props.image} />
           </div>
-        </div>
+            <div className="card-block">
+              <ul>
+                <li>
+                  <strong>Name:</strong> {props.name}
+                </li>
+                <li>
+                  <strong>Activity:</strong> {props.activity}
+                </li>
+                <li>
+                  <strong>Bio:</strong> {props.bio}
+                </li>
+              </ul>
+            </div>
+          </div>
+      </a>
     </div>
 </div>
 
