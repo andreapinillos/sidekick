df204a2b8edb9a0aba23e16c6f6e6621a2eaa0a0
sharklemon <anastasia.l.mark@gmail.com>
2017-10-03 11:24:00 -0400

    try again

--- controllers/sidekickController.js
+++ controllers/sidekickController.js
@@ -3,12 +3,18 @@ const db = require("../models");
 // Defining methods for the sidekickController
 module.exports = {
   findAll: function(req, res) {
-    db.Sidekick_model
-      .find(req.query)
-      .sort({ date: -1 })
+    db.getCollection('Sidekick_model').find({})
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+
+  // findAll: function(req, res) {
+  //   db.Sidekick_model
+  //     .find(req.query)
+  //     .sort({ date: -1 })
+  //     .then(dbModel => res.json(dbModel))
+  //     .catch(err => res.status(422).json(err));
+  // },
   findById: function(req, res) {
     db.Sidekick_model
       .findById(req.params.id)
