759ebcad08ccd140a0bab1e229d2021364a3b080
sharklemon <anastasia.l.mark@gmail.com>
2017-10-02 21:41:28 -0400

    working cards in a grid and filters

--- client/src/components/People/People.js
+++ client/src/components/People/People.js
@@ -7,28 +7,20 @@ const People = props =>
 <div>
     <div className="col-sm-4">
       <div className="card">
-        <img src={'https://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=138961693' + '?size=200x200'}/>
+        <img className="profimg" alt={props.name} src={props.image} />
       </div>
         <div className="card-block">
-          <p>Name, Activity, and zip code here</p>
-        </div>
-    </div>
-
-    <div className="col-sm-4">
-      <div className="card">
-        <img src={'http://vignette1.wikia.nocookie.net/epic-rap-battles-of-cartoons/images/5/5a/Patrick_star_preview_2.png' + '?size=200x200'}/>
-      </div>
-        <div className="card-block">
-          <p>Name, Activity, and zip code here</p>
-        </div>
-    </div>
-
-    <div className="col-sm-4">
-      <div className="card">
-        <img src={'https://vignette.wikia.nocookie.net/spongebob/images/f/f1/Squidward-Mad.gif/revision/latest?cb=20130623062624' + '?size=200x200'}/>
-      </div>
-        <div className="card-block">
-          <p>Name, Activity, and zip code here</p>
+      <ul>
+        <li>
+          <strong>Name:</strong> {props.name}
+        </li>
+        <li>
+          <strong>Activity:</strong> {props.activity}
+        </li>
+        <li>
+          <strong>Bio:</strong> {props.bio}
+        </li>
+      </ul>
         </div>
     </div>
 </div>
