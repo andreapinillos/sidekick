803f4f74ccbaeb430091bdadc774be1207f01773
Dain Lee <dlekdls1028@gmail.com>
2017-10-04 11:30:06 -0400

    People component changed

--- client/src/components/People/People.js
+++ client/src/components/People/People.js
@@ -5,13 +5,13 @@ import "./People.css";
 const People = props =>
 
 <div>
-    <div className="col-sm-4">
+    <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4">
       <a href={props.href}>
-        <div className="cardbox">
+        <div className="cardbox dropdown">
           <div className="card">
             <img className="profimg" alt={props.name} src={props.image} />
           </div>
-            <div className="card-block">
+            <div className="card-block dropdown-content">
               <ul>
                 <li>
                   <strong>Name:</strong> {props.name}
